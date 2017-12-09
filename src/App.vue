a6a1ee29d0675afdc676b2e73abe0cd44aa4e2e4
huangsd <912143546@qq.com>
2017-12-09 13:32:33 +0800

    enviroment

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+  <div class="test" @click='test'></div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import './css/styles.scss'
+export default {
+  name: 'app',
+  methods:{
+    test(){
+      console.log('test')
+    },
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: red;
+  margin-top: 60px;
+}
+</style>
